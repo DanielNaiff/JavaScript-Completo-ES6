4a93f0e55dd3465d4d06f1039b56adfe7e3827d3
danielnaiff <daniel.naiff@yahoo.com.br>
2024-01-17 17:37:57 -0300

    Objetos

---
+++ Objetos/script.js
@@ -0,0 +1,50 @@
+var pessoa = {
+  nome: 'André',
+  idade: 28,
+  profissao: 'Designer',
+  possuiFaculdade: true,
+};
+
+pessoa.nome; // 'André'
+pessoa.possuiFaculdade; // true
+
+var quadrado = {
+  lados: 4,
+  area: function (lado) {
+    return lado * lado;
+  },
+  perimetro: function (lado) {
+    return this.lados * lado;
+  },
+};
+
+quadrado.lados; // 4
+quadrado.area(5); // 25
+quadrado.perimetro(5); // 20
+
+var quadrado = {
+  lados: 4,
+  area(lado) {
+    return lado * lado;
+  },
+  perimetro(lado) {
+    return this.lados * lado;
+  },
+};
+
+var menu = {
+  width: 800,
+  height: 50,
+  backgroundColor: '#84E',
+};
+
+var bg = menu.backgroundColor; // '#84E'
+
+var menu = {
+  width: 800,
+  height: 50,
+  backgroundColor: '#84E',
+};
+
+menu.backgroundColor = '#000';
+console.log(menu.backgroundColor); // '#000'
