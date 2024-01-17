107f7698a9ee9c19ce96f922e2b32e6f0aa6348c
danielnaiff <daniel.naiff@yahoo.com.br>
2024-01-17 16:17:39 -0300

    Variáveis

---
+++ variaveis/script.js
@@ -0,0 +1,20 @@
+var nome = 'Daniel';
+var idade = 24;
+var possuiFaculdade = false;
+
+console.log(nome, idade, possuiFaculdade);
+
+var preco = 25;
+var totalComprado = 5;
+var totalPreco = totalComprado * preco;
+
+console.log(totalPreco);
+
+var sobrenome = 'Rafael',
+  cidade = 'Rio';
+
+console.log(sobrenome, cidade);
+
+var semDefinir;
+
+console.log(semDefinir);
