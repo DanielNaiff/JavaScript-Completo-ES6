4bf1f01ea399d04a5703039d8f51fd027981aa5d
danielnaiff <daniel.naiff@yahoo.com.br>
2024-01-17 17:06:20 -0300

    funções

---
+++ Funções/script.js
@@ -0,0 +1,14 @@
+function areaQuadrado(lado) {
+  return lado;
+}
+
+console.log(areaQuadrado(2));
+
+// peso e altura são os parâmetros
+function imc(peso, altura) {
+  const imc = peso / altura ** 2;
+  return imc;
+}
+
+imc(80, 1.8); // 80 e 1.80 são os argumentos
+imc(60, 1.7); // 60 e 1.70 são os argumentos
