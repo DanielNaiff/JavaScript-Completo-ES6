f46b49bcf0db221a13c84e1934392530dcff26c7
danielnaiff <daniel.naiff@yahoo.com.br>
2024-01-17 16:58:59 -0300

    Boolean e Condicionais

--- Boolean e Condicionais/script.js
+++ Boolean e Condicionais/script.js
@@ -6,3 +6,19 @@ if (possuiGraduacao) {
 } else if (possuiDoutorado) {
   console.log('É verdadeiro');
 }
+
+var corFavorita = 'Azul';
+
+switch (corFavorita) {
+  case 'Azul':
+    console.log('Olhe para o céu.');
+    break;
+  case 'Vermelho':
+    console.log('Olhe para rosas.');
+    break;
+  case 'Amarelo':
+    console.log('Olhe para o sol.');
+    break;
+  default:
+    console.log('Feche os olhos');
+}
