89c12a72e9061bf8970bcc5ab82a90ca8ac553e2
danielnaiff <daniel.naiff@yahoo.com.br>
2024-01-17 16:31:36 -0300

    String

---
+++ Tipos de dados/script.js
@@ -0,0 +1,17 @@
+var nome = 'Daniel';
+console.log(typeof nome);
+
+var sobrenome = 'Naiff';
+
+var nomeCompleto = nome + ' ' + sobrenome;
+
+console.log(nomeCompleto);
+
+var ano = '2018';
+var mes = 8;
+console.log(ano + ' ' + mes);
+
+var gols = 1000;
+var frase = `Romário fez ${gols * 2} gols`;
+
+console.log(frase);
