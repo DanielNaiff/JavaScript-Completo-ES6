032fba19f98538eeaea273f4e5171450c62e2b15
danielnaiff <daniel.naiff@yahoo.com.br>
2024-01-27 16:54:58 -0300

    Adicionando o JS

--- Dom Para Iniciantes/Animais Fantásticos/script.js
+++ Dom Para Iniciantes/Animais Fantásticos/script.js
@@ -1,37 +1,19 @@
-// Verifique a distância da primeira imagem
-// em relação ao topo da página
-const img = document.querySelector('img');
-const imgTop = img.offsetTop;
+const tabMenu = document.querySelectorAll('.js-tabmenu li');
+const tabContent = document.querySelectorAll('.js-tabcontent section');
 
-console.log(imgTop);
-// Retorne a soma da largura de todas as imagens
-function somaImagens() {
-  const imagens = document.querySelectorAll('img');
-  let soma = 0;
-  imagens.forEach(imagem => {
-    soma = soma + imagem.offsetWidth;
-  });
+if (tabMenu.length && tabContent.length) {
+  tabContent[0].classList.add('ativo');
 
-  console.log(soma);
-}
-
-window.onload = function () {
-  somaImagens();
-};
-
-// Verifique se os links da página possuem
-// o mínimo recomendado para telas utilizadas
-// com o dedo. (48px/48px de acordo com o google)
-
-const links = document.querySelectorAll('a');
-
-links.forEach(link => {
-  if ((link.offsetWidth >= 48) & (link.offsetHeight >= 48)) {
-    console.log('Links com padrão correto: ' + link);
-  } else {
-    console.log('Links fora do padrão:' + link);
+  function activeTab(index) {
+    tabContent.forEach(section => {
+      section.classList.remove('ativo');
+    });
+    tabContent[index].classList.add('ativo');
   }
-});
 
-// Se o browser for menor que 720px,
-// adicione a classe menu-mobile ao menu
+  tabMenu.forEach((itemMenu, index) => {
+    itemMenu.addEventListener('click', () => {
+      activeTab(index);
+    });
+  });
+}
