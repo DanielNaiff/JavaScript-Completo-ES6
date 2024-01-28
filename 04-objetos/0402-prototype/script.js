db435bc1f8aec497748b4de85ac050ee09f21163
danielnaiff <daniel.naiff@yahoo.com.br>
2024-01-27 23:59:09 -0300

    prototype

--- 04-objetos/0402-prototype/script.js
+++ 04-objetos/0402-prototype/script.js
@@ -2,37 +2,16 @@
 // Deve conter nome, sobrenome e idade
 // Crie um método no protótipo que retorne
 // o nome completo da pessoa
-
-function Pessoa(nome, sobrenome, idade) {
+function Pessoas(nome, sobrenome, idade) {
   this.nome = nome;
   this.sobrenome = sobrenome;
   this.idade = idade;
-} 
-
-Pessoa.prototype.nomeCompleto = function() {
-  return `${this.nome} ${this.sobrenome}`
 }
 
-const andre = new Pessoa('André', 'Rafael', 28);
+Pessoas.prototype.nomeCompleto = function () {
+  return this.nome + this.sobrenome + this.idade;
+};
 
-// Liste os métodos acessados por 
+// Liste os métodos acessados por
 // dados criados com NodeList,
 // HTMLCollection, Document
-Object.getOwnPropertyNames(NodeList.prototype)
-Object.getOwnPropertyNames(HTMLCollection.prototype)
-Object.getOwnPropertyNames(Document.prototype)
-
-
-// Liste os construtores dos dados abaixo
-const li = document.querySelector('li');
-
-li; // HTMLLIElement
-li.click; // Function
-li.innerText; // String
-li.value; // Number
-li.hidden; // Boolean
-li.offsetLeft; // Number
-li.click(); // undefined
-
-// Qual o construtor do dado abaixo:
-li.hidden.constructor.name; // String
