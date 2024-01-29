eba086ca43d98b1175e99b1ce8b401beb3218392
danielnaiff <daniel.naiff@yahoo.com.br>
2024-01-28 23:12:39 -0300

    array interação

--- 04-objetos/0407-array-e-iteracao/script.js
+++ 04-objetos/0407-array-e-iteracao/script.js
@@ -1,73 +1,62 @@
 // Selecione cada curso e retorne uma array
 // com objetos contendo o título, descricao,
 // aulas e horas de cada curso
-const cursos = document.querySelectorAll('.curso');
-const arrayCursos = Array.from(cursos);
+const curso = document.querySelectorAll('.curso');
 
-const objetosCurso = arrayCursos.map((curso) => {
-  const titulo = curso.querySelector('h1').innerText;
-  const descricao = curso.querySelector('p').innerText;
-  const aulas = curso.querySelector('.aulas').innerText;
-  const horas = curso.querySelector('.horas').innerText;
-  return {
-    titulo,
-    descricao,
-    aulas,
-    horas
-  }
-})
+const objetctCurso = Array.from(curso).map(curso => {
+  const titulo = curso.querySelector('h1').textContent;
+  const descricao = curso.querySelector('p').textContent;
+  const aulas = parseInt(curso.querySelector('.aulas').textContent);
+  const horas = parseInt(curso.querySelector('.horas').textContent);
 
-console.log(objetosCurso);
-console.log(arrayCursos);
-
+  return { titulo, descricao, aulas, horas };
+});
+console.log(objetctCurso);
 
 // Retorne uma lista com os
 // números maiores que 100
 const numeros = [3, 44, 333, 23, 122, 322, 33];
-const maioresQue100 = numeros.filter(n => n > 100);
 
-console.log(maioresQue100)
+const numerosMaioresQue100 = numeros.filter(numero => numero > 100);
+
+console.log(numerosMaioresQue100);
 
 // Verifique se Baixo faz parte
 // da lista de instrumentos e retorne true
-const instrumentos = ['Guitarra', 'Baixo', 'Bateria', 'Teclado']
-const possuiBaixo = instrumentos.some((item) => {
-  return item === 'Baixo';
-})
-
-console.log(possuiBaixo);
+const instrumentos = ['Guitarra', 'Baixo', 'Bateria', 'Teclado'];
+const baixaEsta = instrumentos.includes('Baixo');
+console.log(baixaEsta);
 
 // Retorne o valor total das compras
 const compras = [
   {
     item: 'Banana',
-    preco: 'R$ 4,99'
+    preco: 'R$ 4,99',
   },
   {
     item: 'Ovo',
-    preco: 'R$ 2,99'
+    preco: 'R$ 2,99',
   },
   {
     item: 'Carne',
-    preco: 'R$ 25,49'
+    preco: 'R$ 25,49',
   },
   {
     item: 'Refrigerante',
-    preco: 'R$ 5,35'
+    preco: 'R$ 5,35',
   },
   {
     item: 'Quejo',
-    preco: 'R$ 10,60'
+    preco: 'R$ 10,60',
   },
-  {
-    item: 'Arroz',
-    preco: 'R$ 5,50'
-  }
-]
+];
 
-const valorTotal = compras.reduce((acumulador, item) => {
-  const precoLimpo = +item.preco.replace('R$ ', '').replace(',', '.');
-  return acumulador + precoLimpo;
-}, 0)
+const converterParaNumero = preco =>
+  parseFloat(preco.replace('R$', '').replace(',', '.'));
+
+const valorTotal = compras.reduce(
+  (total, compra) => total + converterParaNumero(compra.preco),
+  0
+);
 
 console.log(valorTotal);
