126a5875fa30e29faa5edf19d7b80e2e33d800cf
danielnaiff <daniel.naiff@yahoo.com.br>
2024-01-28 21:15:43 -0300

    String

--- 04-objetos/0404-string/script.js
+++ 04-objetos/0404-string/script.js
@@ -26,49 +26,40 @@ const transacoes = [
 
 let taxaTotal = 0;
 let recebimentoTotal = 0;
-transacoes.forEach((item) => {
+transacoes.forEach(item => {
   const numeroLimpo = +item.valor.replace('R$ ', '');
   if (item.descricao.slice(0, 4) === 'Taxa') {
     taxaTotal += numeroLimpo;
-  } else if(item.descricao.slice(0, 4) === 'Rece') {
+  } else if (item.descricao.slice(0, 4) === 'Rece') {
     recebimentoTotal += numeroLimpo;
   }
-})
-console.log(taxaTotal)
-console.log(recebimentoTotal)
-
+});
+console.log(taxaTotal);
+console.log(recebimentoTotal);
 
 // Retorne uma array com a lista abaixo
 const transportes = 'Carro;Avião;Trem;Ônibus;Bicicleta';
 
-const arrayTransportes = transportes.split(';');
-console.log(arrayTransportes);
+console.log(transportes.split(';'));
 
 // Substitua todos os span's por a's
-let html = `<ul>
+const html = `<ul>
                 <li><span>Sobre</span></li>
                 <li><span>Produtos</span></li>
                 <li><span>Contato</span></li>
               </ul>`;
 
-html = html.split('span').join('a');
-
-console.log(html);
+const htmla = html.split('span').join('a');
+console.log(htmla);
 
 // Retorne o último caracter da frase
 const frase = 'Melhor do ano!';
 
-console.log(frase.slice(-1));
-
 // Retorne o total de taxas
-const transacoes2 = ['Taxa do Banco', '   TAXA DO PÃO', '  taxa do mercado', 'depósito Bancário', 'TARIFA especial'];
-
-let taxasTotal = 0;
-transacoes2.forEach((item) => {
-  item = item.toLowerCase().trim().slice(0,4);
-
-  if(item === 'taxa')
-    taxasTotal++
-})
-
-console.log(taxasTotal)
+const transacoe = [
+  'Taxa do Banco',
+  '   TAXA DO PÃO',
+  '  taxa do mercado',
+  'depósito Bancário',
+  'TARIFA especial',
+];
