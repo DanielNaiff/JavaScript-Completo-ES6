237fa64ccc913c2f39c4971b1bd921c368c95a54
danielnaiff <daniel.naiff@yahoo.com.br>
2024-01-31 14:57:01 -0300

    Aula 57

---
+++ CFB CURSOS/aula57.js
@@ -0,0 +1,12 @@
+class Pessoa {
+  constructor(prNome) {
+    this.nome = prNome;
+  }
+}
+
+let p1 = new Pessoa('Bruno');
+let p2 = new Pessoa('Daniel');
+let p3 = new Pessoa('Brena');
+console.log(p1.nome);
+console.log(p2.nome);
+console.log(p3.nome);
