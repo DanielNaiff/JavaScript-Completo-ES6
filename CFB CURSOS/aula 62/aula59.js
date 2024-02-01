3f2e2e3c57a2196c4f997a2d6e001e8e52e03bc2
danielnaiff <daniel.naiff@yahoo.com.br>
2024-02-01 14:51:45 -0300

    .

--- CFB CURSOS/aula 62/aula59.js
+++ CFB CURSOS/aula 62/aula59.js
@@ -1,9 +1,25 @@
-const Pessoa = {
-  nome: 'Bruno',
-  getNome: function () {
-    return this.nome;
-  },
-  setNome: function (nome) {
+class Carro {
+  constructor(nome, portas) {
     this.nome = nome;
-  },
-};
+    this.porta = portas;
+    this.ligado = false;
+    this.vel = 0;
+    this.cor = undefined;
+  }
+  ligar = function () {
+    this.ligado = true;
+  };
+  desligar = function () {
+    this.ligado = false;
+  };
+  setCor = function (cor) {
+    this.cor = cor;
+  };
+}
+
+class Militar extends Carro {}
+
+const c1 = new Carro('Normal', 4);
+
+console.log(`Nome: ${c1.nome}`);
+console.log(`Nome: ${c1.portas}`);
